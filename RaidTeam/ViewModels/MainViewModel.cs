712c48f8294506ebdf4bf81fe8bc8574c1764ab8
diegoparradoc96 <diegoparrado.cardozo@hotmail.com>
2025-08-12 17:56:49 -0500

    feature/progress in the implementation of the modal

---
+++ RaidTeam/ViewModels/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using RaidTeam.Services;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+
+namespace RaidTeam.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        private readonly DialogService _dialogService;
+
+        [ObservableProperty]
+        private ObservableCollection<string> jugadores = new();
+
+        public MainViewModel(DialogService dialogService)
+        {
+            _dialogService = dialogService;
+        }
+
+        [RelayCommand]
+        public async Task AddPlayerAsync()
+        {            
+            var nombreJugador = await _dialogService.ShowAddPlayerDialogAsync();
+            if (!string.IsNullOrWhiteSpace(nombreJugador))
+            {
+                Jugadores.Add(nombreJugador);
+            }
+        }
+    }
+}
